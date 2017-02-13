89ad4661b545ef370df7678798323e32a5bf3068
Brian Lu <zhichulu75@gmail.com>
2017-02-12 22:16:02 -0500

    setup.py

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rsystem',
+      version='0.1.0',
+      description='python robot system',
+      author='Brian Lu',
+      author_email='zlu7@cmu.edu',
+      packages=['rsystem', 'rplugin'],
+     )
