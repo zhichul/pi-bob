ec676bcc5c6fe2157e9f327cef4778fbc86253c3
Brian Lu <zhichul@andrew.cmu.edu>
2017-04-14 01:06:48 -0400

    Added ANN implementation

--- src/training/units.py
+++ src/training/units.py
@@ -1,13 +1,20 @@
 import random
 import numpy as np
 
+#for testing
+import cv2 as cv
+import os
+import sys
+import copy
+
 class N21Unit(object):
 	
 	"""
 	An n input 1 output (real or discrete) unit
 	"""
-	def __init__(self,name):
-		self.name = name
+	def __init__(self,ref):
+		assert isinstance(ref,str)
+		self.ref = ref
 		self.upstream = []
 		self.downstream = []
 		self.o = None
@@ -18,10 +25,12 @@ class N21Unit(object):
 		pass
 
 	def addUpstream(self,unit):
-		pass
+		assert isinstance(unit,N21Unit)
+		self.upstream.append(unit)
 
 	def addDownstream(self,unit):
-		pass
+		assert isinstance(unit,N21Unit)
+		self.downstream.append(unit)
 
 	def update(self):
 		pass
@@ -39,11 +48,16 @@ class N21Unit(object):
 
 	@staticmethod
 	def addLink(src,dst):
+		assert isinstance(src, N21Unit)
+		assert isinstance(dst, N21Unit)
 		dst.addUpstream(src)
 		src.addDownstream(dst)
 
 	def __repr__(self):
-		return 'N21Unit("%s")' % self.name
+		return 'N21Unit("%s")' % self.ref
+
+	def __str__(self):
+		return self.__repr__()
 
 
 class LinearUnit(N21Unit):
@@ -51,14 +65,14 @@ class LinearUnit(N21Unit):
 	"""
 	An n input 1 output (real valued) unit
 	"""
-	def __init__(self,name):
-		super(LinearUnit,self).__init__(name)
-		self.weight = [random.random()]
+	def __init__(self,ref):
+		super(LinearUnit,self).__init__(ref)
+		self.weight = [random.random()/20]
 	
 	def verify(self):
 		if __debug__:
 			if not len(self.upstream) > 0:
-				raise AssertionError("%s: A linear unit must have at least one upstream unit." % self.name)
+				raise AssertionError("%s: A linear unit must have at least one upstream unit." % self.ref)
 
 	def addUpstream(self,unit):
 		assert isinstance(unit,N21Unit)
@@ -70,10 +84,15 @@ class LinearUnit(N21Unit):
 		self.downstream.append(unit)
 
 	def update(self):
-		self.o = np.inner(self.weight,[1] + [upstreamUnit.o for upstreamUnit in self.upstream])
+		x = [1]
+		x.extend([upstreamUnit.o for upstreamUnit in self.upstream])
+		self.o = np.inner(self.weight,x)
 
 	def __repr__(self):
-		return 'LinearUnit("%s")' % self.name
+		return 'LinearUnit("%s")' % self.ref
+
+	def __str__(self):
+		return self.__repr__()
 
 class ThresholdUnit(LinearUnit):
 
@@ -86,7 +105,10 @@ class ThresholdUnit(LinearUnit):
 		self.o = 1 if self.o > 0 else -1
 
 	def __repr__(self):
-		return 'ThresholdUnit("%s")' % self.name
+		return 'ThresholdUnit("%s")' % self.ref
+
+	def __str__(self):
+		return self.__repr__()
 
 class SigmoidUnit(LinearUnit):
 
@@ -99,7 +121,10 @@ class SigmoidUnit(LinearUnit):
 		self.o = 1 / (1 + np.exp(-1*self.o))
 
 	def __repr__(self):
-		return 'SigmoidUnit("%s")' % self.name
+		return 'SigmoidUnit("%s")' % self.ref
+
+	def __str__(self):
+		return self.__repr__()
 
 class InputUnit(N21Unit):
 
@@ -107,8 +132,137 @@ class InputUnit(N21Unit):
 	An input unit with no fan-in only fan-out
 	"""
 	def __repr__(self):
-		return 'InputUnit("%s")' % self.name
+		return 'InputUnit("%s")' % self.ref
 
+	def __str__(self):
+		return self.__repr__()
+
+class Layer(object):
+
+	"""
+	A layer of computational units
+	"""
+	def __init__(self,ref,size,unitClass="SigmoidUnit"):
+		self.unitClassName = unitClass
+		unitClass = parseClass(unitClass)
+		self.ref = ref
+		self.units = [unitClass(ref+"U%d"%i)for i in range(size)]
+
+	def update(self):
+		for unit in self.units:
+			unit.update()
+
+	@staticmethod
+	def addLink(src,dst):
+		assert isinstance(src, Layer)
+		assert isinstance(dst, Layer)
+		for upstreamUnit in src.units:
+			upstreamUnit.addAllDownstream(dst.units)
+		for downstreamUnit in dst.units:
+			downstreamUnit.addAllUpstream(src.units)
+
+	def __repr__(self):
+		return 'Layer("%s",%d,unitClass="%s")' % self.unitClassName
+
+	def __str__(self):
+		return self.__repr__()
+
+class InputLayer(Layer):
+
+	def __init__(self,ref,size):
+		super(InputLayer,self).__init__(ref,size,unitClass="InputUnit")
+
+	def update(self,v):
+		assert len(v) == len(self.units)
+		for i, unit in enumerate(self.units):
+			unit.o = v[i]
+
+
+class Network(object):
+
+	def __init__(self,ref,learningRate,sizes,hiddenClass="SigmoidUnit",outputClass="SigmoidUnit"):
+		assert len(sizes) >= 2
+		layerCount = len(sizes)
+		self.hiddenClass = hiddenClass
+		self.outputClass = outputClass
+		self.ref = ref
+		self.rate = learningRate
+		self.layers = []
+		self.layers.append(InputLayer("L0",sizes[0]))
+		self.layers += [Layer("L%d"%i,sizes[i],unitClass=hiddenClass) for i in range(1,layerCount-1)]
+		self.layers.append(Layer("L%d"%(layerCount-1),sizes[-1],unitClass=outputClass))
+		self.o = [None] * sizes[-1]
+		for i in range(len(self.layers)-1):
+			Layer.addLink(self.layers[i],self.layers[i+1])
+
+	def update(self,x):
+		self.layers[0].update(x)
+		for i in range(1,len(self.layers)):
+			self.layers[i].update()
+		self.o = tuple([unit.o for unit in self.layers[-1].units])
+
+	def backProp(self,x,t):
+		# compute delta for output layer
+		for i, unit in enumerate(self.layers[-1].units):
+			unit.delta = unit.o*(1-unit.o)*(t[i]-unit.o)
+		# compute delta for hidden layers if any
+		for i in range(1,len(self.layers)-1):
+			layer = self.layers[i]
+			for index,unit in enumerate(layer.units):
+				unit.delta = unit.o * (1-unit.o) * np.sum([downstreamUnit.weight[index] * downstreamUnit.delta for downstreamUnit in unit.downstream])
+		#update weights
+		for i in range(1,len(self.layers)-1):
+			layer = self.layers[i]
+			for unit in layer.units:
+				for i in range(1,len(unit.weight)):
+					unit.weight[i] += unit.delta * self.rate * unit.upstream[i-1].o
+
+	def train(self,trainingSet,testSet):
+		error = self.error(testSet)
+		lasterror = error
+		i = 0
+		while (error > 0):
+			print("Iter[%d] - Error=%.10f, dE=%.10f. ErrorRate=%.10f" % (i, error, error-lasterror, self.errorRate(testSet)))
+			for x,t in trainingSet:
+				self.update(x)
+				self.backProp(x,t)
+			lasterror = error
+			error = self.error(testSet)
+			i += 1
+
+	def error(self,testSet):
+		error = 0
+		for instance,t in testSet:
+			self.update(instance)
+			error += sum(0.5 * np.square(np.subtract(t,self.o)))
+		return error
+
+	def errorRate(self,testSet):
+		error = 0
+		for instance,t in testSet:
+			self.update(instance)
+			if self.o.index(max(self.o)) != t.index(max(t)):
+				error += 1
+		return error/len(testSet)
+
+
+	def __str__(self):
+		s =  ("[----Out] <L%d> | " % (len(self.layers)-1)) + str(self.layers[-1].units[0]) + " ... " + str(self.layers[-1].units[-1]) + "\n"
+		for i in range(1,len(self.layers)-1):
+			s += ("[-Hidden] <L%d> | "  % i)+ str(self.layers[i].units[0]) + " ... " + str(self.layers[i].units[-1]) + "\n"
+		s += "[--Input] <L0> | " + str(self.layers[0].units[0]) + " ... " + str(self.layers[0].units[-1])
+		return s
+
+def parseClass(unitClass):
+	if unitClass == "ThresholdUnit":
+		unitClass = ThreasholdUnit
+	elif unitClass == "SigmoidUnit":
+		unitClass = SigmoidUnit
+	elif unitClass == "InputUnit":
+		unitClass = InputUnit
+	else:
+		raise AssertionError("Invalid unitClass")
+	return unitClass
 
 def testUnits():
 	A = LinearUnit("A")
@@ -138,8 +292,8 @@ def testUnits():
 	assert A.o == 4
 	# test addUpstream/Downstream
 	n = 10
-	l1 = [LinearUnit(i) for i in range(n)]
-	l2 = [LinearUnit(i) for i in range(2*n)]
+	l1 = [LinearUnit(str(i)) for i in range(n)]
+	l2 = [LinearUnit(str(i)) for i in range(2*n)]
 	for inputUnit in l1:
 		inputUnit.addAllDownstream(l2)
 	for outputUnit in l2:
@@ -177,6 +331,53 @@ def testUnits():
 
 	print("Test on Linear/Threashold/SigmoidUnits passed.")
 
+def testNetwork():
+	ANN = Network("ANN",0.05,(64*48,4,3))
+	for inputUnit in ANN.layers[0].units:
+		assert inputUnit.upstream == []
+		assert inputUnit.downstream == ANN.layers[1].units
+	for hiddenUnit in ANN.layers[1].units:
+		assert hiddenUnit.upstream == ANN.layers[0].units
+		assert hiddenUnit.downstream == ANN.layers[2].units
+	for outputUnit in ANN.layers[2].units:
+		assert outputUnit.downstream == []
+		assert outputUnit.upstream == ANN.layers[1].units
+	ANN.update(np.random.rand(64*48))
+	ANN.update(np.zeros(64*48))
+	print("Test on Network passed.")
+
+def main():
+
+	assert len(sys.argv) > 1
+	assert os.path.isdir(sys.argv[1])
+	data = parseData(sys.argv[1])
+	ANN = Network("ANN",0.25,(64*48,4,3))
+	D = random.sample(data,len(data)//2)
+	T = [x for x in data if x in D]
+	ANN.train(D,T)
+
+
+def parseData(path):
+
+	res = []
+	for file in os.listdir(path):
+		img = cv.imread(os.path.join(path,file),0)
+		decision = file.split("-")[0]
+		t = [0] * 3
+		if decision == "left":
+			t[0] = 1
+		elif decision == "right":
+			t[2] = 1
+		elif decision == "straight":
+			t[1] = 1
+		else:
+			print("Unidentified training example: %s" % file)
+		res.append((tuple(np.multiply(1/255000,np.ndarray.flatten(img)).tolist()),tuple(t)))
+	return res
+
+	ANN = Network("ANN",0.05,(64*48,4,3))
 if __name__ == "__main__":
-	testUnits()
+	# testUnits()
+	# testNetwork()
+	main()
 
